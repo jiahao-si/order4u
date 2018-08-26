5662796c646bf6f1ec157c7b7e0c397ac4fb9b27
nolansi <nolansi@tencent.com>
2018-08-26 18:05:23 +0800

    前端路由

--- develop/server/server.ts
+++ develop/server/server.ts
@@ -1,46 +1,55 @@
-
-import * as path from "path";
-import * as express from "express";
-import * as webpack from "webpack";
-import * as serveIndex from "serve-index";
+import * as path from 'path'
+import * as express from 'express'
+import * as webpack from 'webpack'
+import * as serveIndex from 'serve-index'
 
 export function serve(port?: any) {
-  port = port || process.env.PORT;
+	port = port || process.env.PORT
 
-  var app = express();
+	var app = express()
 
-  setupWebpackDevelopServer(app);
+	setupWebpackDevelopServer(app)
 
-  app.use('/', express.static(path.resolve(__dirname, "../../")));
-  app.use('/static', express.static(path.resolve(__dirname, "../../static")));
+	app.use('/', express.static(path.resolve(__dirname, '../../')))
+	app.use('/static', express.static(path.resolve(__dirname, '../../static')))
 
-  app.listen(port, function (err) {
-    if (err) {
-      console.error(JSON.stringify(err));
-      return;
-    }
+	app.use('/home', function(req, res) {
+		res.send(
+			'<html><head>...head<script src="http://localhost:8089/static/main.js"></script></head><body></body></html>'
+		)
+	})
 
-    console.log(`\nQCloud development server served at http://localhost:${port}\n\n`);
-  });
+	app.listen(port, function(err) {
+		if (err) {
+			console.error(JSON.stringify(err))
+			return
+		}
 
-  return app;
+		console.log(
+			`\nQCloud development server served at http://localhost:${port}\n\n`
+		)
+	})
+
+	return app
 }
 
 function setupWebpackDevelopServer(app: express.Express) {
-  var config = require('../../webpack/webpack.config');
-  var compiler = webpack(config);
+	var config = require('../../webpack/webpack.config')
+	var compiler = webpack(config)
 
-  var devMiddleware = require('webpack-dev-middleware')(compiler, {
-    publicPath: config.output.publicPath,
-    noInfo: true,
-    stats: { colors: true },
-    poll: true,
-    quiet: false,
-    reload: true
-  });
+	var devMiddleware = require('webpack-dev-middleware')(compiler, {
+		publicPath: config.output.publicPath,
+		noInfo: true,
+		stats: { colors: true },
+		poll: true,
+		quiet: false,
+		reload: true
+	})
 
-  var hotMiddleware = require('webpack-hot-middleware')(compiler, { reload: true });
+	var hotMiddleware = require('webpack-hot-middleware')(compiler, {
+		reload: true
+	})
 
-  app.use(devMiddleware);
-  app.use(hotMiddleware);
+	app.use(devMiddleware)
+	app.use(hotMiddleware)
 }
